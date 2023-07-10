0d13c7a910eb52ac1a07d27243f69cc88fa6bbcf
Скиба Євген <yevhen.skyba@ligazakon.ua>
2023-07-10 11:46:46 +0300

    started work with embeded solr

---
+++ emded_solr_test/src/test/java/sample/index/IntegrationTest.java
@@ -0,0 +1,7 @@
+package sample.index;
+import org.apache.solr.client.solrj.SolrClient;
+public class IntegrationTest {
+
+    static final String CONFIGSET_DIR = "src/test/resources/configsets";
+    private static SolrClient solrClient;
+}
